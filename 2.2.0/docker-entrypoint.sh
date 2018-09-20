f581380970bcd0d22d50e68957b0a5d5896a2d3c
Alfredo Matas <amatas@gmail.com>
2018-09-20 18:40:11 +0200

    Fix identation and create file before set the permissions

--- 2.2.0/docker-entrypoint.sh
+++ 2.2.0/docker-entrypoint.sh
@@ -25,22 +25,22 @@ if [ "$1" = 'couchdb' ]; then
 fi
 
 if [ "$1" = '/opt/couchdb/bin/couchdb' ]; then
+	if [ -n "$ERLANG_COOKIE" ]; then
+		echo $ERLANG_COOKIE > /opt/couchdb/.erlang.cookie
+		chmod 600 /opt/couchdb/.erlang.cookie
+	fi
+
 	# we need to set the permissions here because docker mounts volumes as root
 	chown -fR couchdb:couchdb /opt/couchdb || true
 
 	chmod -fR 0770 /opt/couchdb/data || true
 
-        find /opt/couchdb/etc -name \*.ini -exec chmod -f 664 {} \;
+	find /opt/couchdb/etc -name \*.ini -exec chmod -f 664 {} \;
 	chmod -f 775 /opt/couchdb/etc/*.d || true
 
-	if [ -n "$ERLANG_COOKIE" ]; then
-		echo $ERLANG_COOKIE > /opt/couchdb/.erlang.cookie
-		chmod 600 /opt/couchdb/.erlang.cookie
-    fi
-
- 	if [ -z "$NODENAME" ]; then
+	if [ -z "$NODENAME" ]; then
 		NODENAME=$(hostname -f)
-    fi
+	fi
 
 	if [ ! -z "$NODENAME" ] && ! grep "couchdb@" /opt/couchdb/etc/vm.args; then
 		echo "-name couchdb@$NODENAME" >> /opt/couchdb/etc/vm.args
